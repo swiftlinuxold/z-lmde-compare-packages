b557d37b6046bb7313b43ca10eb6bb3b1912dcb3
Jason Hsu <github@jasonhsu.com>
2012-01-16 16:21:06 -0600

    Mint 9 GNOME vs. others

--- compare.py
+++ compare.py
@@ -15,13 +15,23 @@ def to_plist (plist1, file1):
 
 lmde_gnome = plist('input/lmde-gnome.txt')
 lmde_xfce = plist('input/lmde-xfce.txt')
+
 only_lmde_gnome = lmde_gnome - lmde_xfce
 to_plist (only_lmde_gnome, 'only_lmde_gnome.txt')
+
+only_lmde_xfce = lmde_xfce - lmde_gnome
+to_plist (only_lmde_xfce, 'only_lmde_xfce.txt')
 
 mint9_gnome = plist('input/mint9-gnome.txt')
 mint9_lxde = plist('input/mint9-lxde.txt')
 mint9_fluxbox = plist('input/mint9-fluxbox.txt')
+
 only_mint9_gnome = mint9_gnome - mint9_lxde - mint9_fluxbox
 to_plist (only_mint9_gnome, 'only_mint9_gnome.txt')
 
+mint9_gnome_not_lxde = mint9_gnome - mint9_lxde
+to_plist (mint9_gnome_not_lxde, 'mint9_gnome_not_lxde.txt')
+
+mint9_gnome_not_fluxbox = mint9_gnome - mint9_fluxbox
+to_plist (mint9_gnome_not_fluxbox, 'mint9_gnome_not_fluxbox.txt')
 
